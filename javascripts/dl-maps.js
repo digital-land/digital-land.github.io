5b716064feafb65f253aed42d7f41dbf98ba67fe
Travis CI User <travis@example.org>
2020-12-23 19:33:55 +0000

    rebuilding site Wed Dec 23 19:33:55 UTC 2020

--- javascripts/dl-maps.js
+++ javascripts/dl-maps.js
@@ -260,7 +260,7 @@ Map.prototype.setupOptions = function (params) {
     default_pos: params.default_pos || [52.561928, -1.464854],
     default_zoom: params.minZoom || 6,
     minZoom: params.minZoom || 6,
-    maxZoom: params.maxZoom || 16
+    maxZoom: params.maxZoom || 18
   };
   this.mapId = params.mapId || 'aMap';
 };
